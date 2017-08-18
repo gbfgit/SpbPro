2ada6de39674503ec7521cd64fbaaf3c3a33650c
gbfgit <30331858+gbfgit@users.noreply.github.com>
2017-08-18 19:11:45 +0800

    Add files via upload

--- Test01SpringBoot02/src/main/java/com/roo/demo/Test01SpringBoot02Application.java
+++ Test01SpringBoot02/src/main/java/com/roo/demo/Test01SpringBoot02Application.java
@@ -1,5 +1,6 @@
 package com.roo.demo;
 
+import com.roo.demo.entity.Author;
 import com.roo.demo.entity.BookBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,6 +23,8 @@ public class Test01SpringBoot02Application {
 	//如果在实体类中(实体类不是在创建工程是的目录或子目录下)使用@Component和@ConfigurationProperties注解，则无法使用自动注入注解
 	@Autowired
 	private BookBean bookBean;
+	@Autowired
+	private Author author;
 	public static void main(String[] args) {
 		SpringApplication.run(Test01SpringBoot02Application.class, args);
 	}
@@ -30,6 +33,7 @@ public class Test01SpringBoot02Application {
 //		return "GitGbf's First Spring Boot Project With IDEA \r\nBook Name = " + bookName
 //				+ " Book Author = " + bookAuthor + " Book Price = " + bookPrice;
 		return "Book Name = " + bookBean.getName() + " Book Author = " + bookBean.getAuthor() + " Book Price = "
-				+ bookBean.getPrice();
+				+ bookBean.getPrice() + " Author Name = " + author.getName() + " Author Gender = " + author.getGender()
+				+ " Author Age = " + author.getAge();
 	}
 }
